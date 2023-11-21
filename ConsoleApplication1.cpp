bceef9eaf8546077c976e56ab511ca66f5b8b15b
Georgy-Fomk1n <144357211+Georgy-Fomk1n@users.noreply.github.com>
2023-11-21 12:34:10 +0300

    Указатели и структуры

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+
+struct node {
+    string name;
+    int age;
+    struct node* next = NULL;
+};
+
+node* idperson(node* pointer, string name, int id) {
+    int j = 1;
+    struct node* link = pointer;
+    struct node* person = new node;
+    person->name = name;
+
+    if (id == 1 || pointer == NULL) {
+        person->next = pointer;
+        return person;
+    }
+    while (pointer->next != NULL && j < id - 1) {
+        pointer = pointer->next;
+        j++;
+    }
+    person->next = pointer->next;
+    pointer->next = person;
+    return link;
+}
+
+node* del_idperson(node* pointer, int id) {
+    int j = 1;
+    struct node* link = pointer, * lnk_del;
+
+    if (pointer == NULL)
+        return pointer;
+    if (id == 1) {
+        lnk_del = pointer;
+        pointer = pointer->next;
+        delete lnk_del;
+        return pointer;
+    }
+    while (pointer->next != NULL && j < id - 1) {
+        pointer = pointer->next;
+        j++;
+    }
+    lnk_del = pointer->next;
+    if (pointer->next != nullptr) {
+        pointer->next = lnk_del->next;
+        delete lnk_del;
+        return link;
+    }
+    
+}
+
+    int main() {
+        setlocale(LC_ALL, "");
+        struct node kate;
+        kate.name = "Kate";
+        struct node tom;
+        tom.name = "Tom";
+        struct node bob;
+        bob.name = "Bob";
+        kate.next = &tom;
+        tom.next = &bob;
+        cout << "1. Добавить" << endl;
+        cout << "2. Убрать" << endl;
+}
+
+    
